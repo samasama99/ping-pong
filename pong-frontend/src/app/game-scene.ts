4da688252b62717572fc3a3aa10bf9ee859f917a
sama-sama <test@test.com>
2023-06-07 11:15:34 +0100

    trying to run Phaser

--- pong-frontend/src/app/game-scene.ts
+++ pong-frontend/src/app/game-scene.ts
@@ -1,6 +1,8 @@
 import Phaser from 'phaser';
 
 export class GameScene extends Phaser.Scene {
+  private paddle!: Phaser.GameObjects.Graphics;
+
   constructor() {
     super({ key: 'game' });
   }
@@ -12,6 +14,11 @@ export class GameScene extends Phaser.Scene {
   create() {
     // Set up game objects and initial state here
     console.log('create method');
+    this.paddle = this.add.graphics();
+    this.paddle.fillStyle(0xff0000);
+    this.paddle.fillRect(0, 0, 10, 10);
+    this.paddle.x = 100;
+    this.paddle.y = 100;
   }
 
   override update() {
